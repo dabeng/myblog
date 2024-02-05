7c97b5b3a715df2b7b7473aa9c1828160a223d14
dabeng <dabeng413@gmail.com>
2024-02-05 17:05:15 +0800

    add reactions

--- src/modules/Blogs/Comments/Reactions.tsx
+++ src/modules/Blogs/Comments/Reactions.tsx
@@ -2,6 +2,62 @@ export default function Reactions() {
   return (
     <div>
       <p>This is the Reactions.</p>
+      <nav className="level">
+        <div className="level-item has-text-centered">
+          <div>
+            <p className="heading">
+              <i className="fa-regular fa-thumbs-up fa-4x"></i>
+            </p>
+            <p className="title is-4">18</p>
+            <p className="heading">Upvote</p>
+          </div>
+        </div>
+        <div className="level-item has-text-centered">
+          <div>
+            <p className="heading">
+              <i className="fa-regular fa-face-grin-tongue-squint fa-4x"></i>
+            </p>
+            <p className="title is-4">1</p>
+            <p className="heading">Funny</p>
+          </div>
+        </div>
+        <div className="level-item has-text-centered">
+          <div>
+            <p className="heading">
+              <i className="fa-regular fa-face-kiss-wink-heart fa-4x"></i>
+            </p>
+            <p className="title is-4">8</p>
+            <p className="heading">Love</p>
+          </div>
+        </div>
+        <div className="level-item has-text-centered">
+          <div>
+            <p className="heading">
+              <i className="fa-regular fa-face-surprise fa-4x"></i>
+            </p>
+            <p className="title is-4">1</p>
+            <p className="heading">Surprised</p>
+          </div>
+        </div>
+        <div className="level-item has-text-centered">
+          <div>
+            <p className="heading">
+              <i className="fa-regular fa-face-angry fa-4x"></i>
+            </p>
+            <p className="title is-4">0</p>
+            <p className="heading">Angry</p>
+          </div>
+        </div>
+        <div className="level-item has-text-centered">
+          <div>
+            <p className="heading">
+              <i className="fa-regular fa-face-sad-tear fa-4x"></i>
+            </p>
+            <p className="title is-4">6</p>
+            <p className="heading">Sad</p>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
