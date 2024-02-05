fbbcbd949c286098ea65b944f10ee95c791b82b6
dabeng <dabeng413@gmail.com>
2024-02-05 16:25:20 +0800

    add commet list prototype

--- src/modules/Blogs/Comments/CommentList.tsx
+++ src/modules/Blogs/Comments/CommentList.tsx
@@ -1,7 +1,51 @@
 export default function CommentList() {
   return (
     <div>
-      <p>This is the List.</p>
+      <p className="title is-6" style={{marginBottom: "-2rem"}}>37 Comments</p>
+      <div className="tabs is-right">
+        <ul>
+          <li className="is-active"><a>Best</a></li>
+          <li><a>Newest</a></li>
+          <li><a>Oldest</a></li>
+        </ul>
+      </div>
+      <article className="media">
+        <figure className="media-left">
+          <p className="image is-64x64">
+            <img src="https://bulma.io/images/placeholders/128x128.png" />
+          </p>
+        </figure>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>Barbara Middleton</strong>
+              <br />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta eros lacus, nec ultricies elit blandit non. Suspendisse pellentesque mauris sit amet dolor blandit rutrum. Nunc in tempus turpis.
+              <br />
+              <small><a>Like</a> · <a>Reply</a> · 3 hrs</small>
+            </p>
+          </div>
+
+          <article className="media">
+            <figure className="media-left">
+              <p className="image is-48x48">
+                <img src="https://bulma.io/images/placeholders/96x96.png" />
+              </p>
+            </figure>
+            <div className="media-content">
+              <div className="content">
+                <p>
+                  <strong>Sean Brown</strong>
+                  <br />
+                  Donec sollicitudin urna eget eros malesuada sagittis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam blandit nisl a nulla sagittis, a lobortis leo feugiat.
+                  <br />
+                  <small><a>Like</a> · <a>Reply</a> · 2 hrs</small>
+                </p>
+              </div>
+            </div>
+          </article>
+        </div>
+      </article>
     </div>
   );
 }
