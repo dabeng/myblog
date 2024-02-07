5979fcefa9d3444c9d125d4e4d7f24ca229214d6
dabeng <dabeng413@gmail.com>
2024-02-07 08:25:37 +0800

    add comments and replies to dd

--- src/shared/db.js
+++ src/shared/db.js
@@ -1,5 +1,7 @@
 import Dexie from 'dexie';
 export const db = new Dexie('database');  // database is database name with 1 version 
 db.version(1).stores({
-  blogs: '++id, title , subtitle, content, publishedDate, updatedDate', // Primary key and indexed props
+  blogs: '++id, title , subtitle, content, publishedDate, updatedDate',// TODO: add author
+  comments: '++id, blogId, content, publishedDate',// TODO: add author
+  replies: '++id, commentId, content, publishedDate'// TODO: add author
 });
