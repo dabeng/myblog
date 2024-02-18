d1298bf28040de052fb8bd3c79ca3e9f6d6dd1e5
dabeng <dabeng413@gmail.com>
2024-02-18 23:18:50 +0800

    add toggle button of comments

--- src/modules/Blogs/Comments/CommentList.tsx
+++ src/modules/Blogs/Comments/CommentList.tsx
@@ -1,5 +1,6 @@
 import { db } from '../../../shared/db';
 import { useState } from 'react';
+import classNames from 'classnames';
 import { useLiveQuery } from 'dexie-react-hooks';
 import CommentBox from './CommentBox';
 import Markdown from 'react-markdown';
@@ -38,12 +39,18 @@ export default function CommentList({ blogId }) {
       }
     });
     //【3】按照用户指定的排序规则，对top level评论和second level评论进行排序
+    const commentCollapsedInitVal = [];
     topLevel.sort(sortToplevelComments);
     topLevel.forEach((tc, index) => {
       tc?.subComments?.sort(sortSecondlevelComments);
+      commentCollapsedInitVal.push([false]);
+      if (tc.subComments) {
+        commentCollapsedInitVal[index].push(Array.from(Array(tc.subComments.length), () => false));
+      }
     });
     //【4】最后，附带生成控制注释框显示/隐藏的标志位数组
     setCommentBoxOpen(Array.from(Array(topLevel.length), () => false));
+    setCommentCollapsed(commentCollapsedInitVal);
     return topLevel;
   };
   const criterionFunction = (comment) => {
@@ -65,6 +72,24 @@ export default function CommentList({ blogId }) {
       } else {
         return open;
       }
+    }));
+  };
+
+  const [commentCollapsed, setCommentCollapsed] = useState([]);
+  const toggleToplevelCommentCollapsed = function (index) {
+    setCommentCollapsed(commentCollapsed.map((tc, i) => {
+      if (i === index) {
+        tc[0] = !tc[0];
+      }
+      return tc;
+    }));
+  };
+  const toggleSecondlevelCommentCollapsed = function (tIndex, sIndex) {
+    setCommentCollapsed(commentCollapsed.map((tc, i) => {
+      if (i === tIndex) {
+        tc[1][sIndex] = !tc[1][sIndex];
+      }
+      return tc;
     }));
   };
 
@@ -91,9 +116,13 @@ export default function CommentList({ blogId }) {
                 <div className="is-flex is-justify-content-space-between">
                   <span className="comment-author is-size-5">Author</span>
                   <p className="buttons">
-                    <button className="button is-info is-inverted">
+                    <button className="button is-info is-inverted" onClick={() => { toggleToplevelCommentCollapsed(index); }}>
                       <span className="icon">
-                        <i className="fa-solid fa-minus"></i>
+                        <i className={classNames({
+                          "fa-solid": true,
+                          "fa-plus": commentCollapsed[index][0],
+                          "fa-minus": !commentCollapsed[index][0]
+                        })}></i>
                       </span>
                     </button>
                     <button className="button is-info is-inverted">
@@ -107,86 +136,102 @@ export default function CommentList({ blogId }) {
                   <time className="comment-published-date is-size-7 has-text-weight-bold has-text-grey">{(new Date(comment.publishedDate)).toLocaleDateString('zh-Hans-CN')}</time>
                 </div>
               </header>
-              <div className="comment-body">
-                <Markdown remarkPlugins={[remarkGfm]}>{comment.content}</Markdown>
-              </div>
-              <footer className="comment-footer">
+              {!commentCollapsed[index][0] &&
+                <>
+                  <div className="comment-body">
+                    <Markdown remarkPlugins={[remarkGfm]}>{comment.content}</Markdown>
+                  </div>
+                  <footer className="comment-footer">
+                    <p className="buttons">
+                      <button className="button is-info is-inverted">
+                        <span className="icon">
+                          <i className="fa-regular fa-thumbs-up"></i>
+                        </span>
+                        <span className="upvote-count">0</span>
+                      </button>
+                      <button className="button is-info is-inverted">
+                        <span className="icon">
+                          <i className="fa-regular fa-thumbs-down"></i>
+                        </span>
+                        <span className="downvote-count">0</span>
+                      </button>
+                    </p>
+                  </footer>
+                </>
+              }
+            </div>
+            {!commentCollapsed[index][0] &&
+              <>
+                {comment?.subComments?.map((subComment, i) => (
+                  <article className="media" key={i}>
+                    <figure className="media-left">
+                      <p className="image is-48x48 has-text-centered">
+                        <i className="fa-solid fa-user fa-3x"></i>
+                      </p>
+                    </figure>
+                    <div className="media-content">
+                      <div className="content">
+                        <header className="comment-header">
+                          <div className="is-flex is-justify-content-space-between">
+                            <span className="comment-author is-size-5">Author</span>
+                            <p className="buttons">
+                              <button className="button is-info is-inverted" onClick={() => { toggleSecondlevelCommentCollapsed(index, i); }}>
+                                <span className="icon">
+                                  <i className={classNames({
+                                    "fa-solid": true,
+                                    "fa-plus": commentCollapsed[index][1][i],
+                                    "fa-minus": !commentCollapsed[index][1][i]
+                                  })}></i>
+                                </span>
+                              </button>
+                              <button className="button is-info is-inverted">
+                                <span className="icon">
+                                  <i className="fa-solid fa-flag"></i>
+                                </span>
+                              </button>
+                            </p>
+                          </div>
+                          <div>
+                            <time className="comment-published-date is-size-7 has-text-weight-bold has-text-grey">{(new Date(subComment.publishedDate)).toLocaleDateString('zh-Hans-CN')}</time>
+                          </div>
+                        </header>
+                        {!commentCollapsed[index][1][i] &&
+                          <>
+                            <div className="comment-body">
+                              <Markdown remarkPlugins={[remarkGfm]}>{subComment.content}</Markdown>
+                            </div>
+                            <footer className="comment-footer">
+                              <p className="buttons">
+                                <button className="button is-info is-inverted">
+                                  <span className="icon">
+                                    <i className="fa-regular fa-thumbs-up"></i>
+                                  </span>
+                                  <span className="upvote-count">0</span>
+                                </button>
+                                <button className="button is-info is-inverted">
+                                  <span className="icon">
+                                    <i className="fa-regular fa-thumbs-down"></i>
+                                  </span>
+                                  <span className="downvote-count">0</span>
+                                </button>
+                              </p>
+                            </footer>
+                          </>
+                        }
+                      </div>
+                    </div>
+                  </article>
+                ))}
                 <p className="buttons">
-                  <button className="button is-info is-inverted">
-                    <span className="icon">
-                      <i className="fa-regular fa-thumbs-up"></i>
-                    </span>
-                    <span className="upvote-count">0</span>
-                  </button>
-                  <button className="button is-info is-inverted">
-                    <span className="icon">
-                      <i className="fa-regular fa-thumbs-down"></i>
-                    </span>
-                    <span className="downvote-count">0</span>
+                  <button className="button is-info is-inverted" onClick={() => { toggleSubCommentBox(index); }}>
+                    <span>Reply</span>
                   </button>
                 </p>
-              </footer>
-            </div>
-            {comment?.subComments?.map((subComment, i) => (
-              <article className="media" key={i}>
-                <figure className="media-left">
-                  <p className="image is-48x48 has-text-centered">
-                    <i className="fa-solid fa-user fa-3x"></i>
-                  </p>
-                </figure>
-                <div className="media-content">
-                  <div className="content">
-                    <header className="comment-header">
-                      <div className="is-flex is-justify-content-space-between">
-                        <span className="comment-author is-size-5">Author</span>
-                        <p className="buttons">
-                          <button className="button is-info is-inverted">
-                            <span className="icon">
-                              <i className="fa-solid fa-minus"></i>
-                            </span>
-                          </button>
-                          <button className="button is-info is-inverted">
-                            <span className="icon">
-                              <i className="fa-solid fa-flag"></i>
-                            </span>
-                          </button>
-                        </p>
-                      </div>
-                      <div>
-                        <time className="comment-published-date is-size-7 has-text-weight-bold has-text-grey">{(new Date(subComment.publishedDate)).toLocaleDateString('zh-Hans-CN')}</time>
-                      </div>
-                    </header>
-                    <div className="comment-body">
-                      <Markdown remarkPlugins={[remarkGfm]}>{subComment.content}</Markdown>
-                    </div>
-                    <footer className="comment-footer">
-                      <p className="buttons">
-                        <button className="button is-info is-inverted">
-                          <span className="icon">
-                            <i className="fa-regular fa-thumbs-up"></i>
-                          </span>
-                          <span className="upvote-count">0</span>
-                        </button>
-                        <button className="button is-info is-inverted">
-                          <span className="icon">
-                            <i className="fa-regular fa-thumbs-down"></i>
-                          </span>
-                          <span className="downvote-count">0</span>
-                        </button>
-                      </p>
-                    </footer>
-                  </div>
-                </div>
-              </article>
-            ))}
-            <p className="buttons">
-              <button className="button is-info is-inverted" onClick={() => { toggleSubCommentBox(index); }}>
-                <span>Reply</span>
-              </button>
-            </p>
-            {commentBoxOpen[index] && (
-              <CommentBox blogId={Number.parseInt(blogId)} parentCommentId={comment.id} />
-            )}
+                {commentBoxOpen[index] && (
+                  <CommentBox blogId={Number.parseInt(blogId)} parentCommentId={comment.id} />
+                )}
+              </>
+            }
           </div>
         </article>
       ))}
