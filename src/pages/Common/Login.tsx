47e62c2a625dc6098c0cec1c28a4e9366acabff7
dabeng <dabeng413@gmail.com>
2024-03-13 22:43:39 +0800

    WIP: add login page

--- src/pages/Common/Login.tsx
+++ src/pages/Common/Login.tsx
@@ -15,7 +15,32 @@ const Login = () => {
     handleLogin();
   }, 3 * 1000);
 
-  return <>Login Page</>;
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => alert(JSON.stringify(data));
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="field">
+        <label className="label">Username</label>
+        <div className="control">
+          <input className="input" type="text" {...register("username")} />
+        </div>
+      </div>
+      <div className="field">
+        <label className="label">Password</label>
+        <div className="control">
+          <input className="input" type="password" {...register("password")} />
+        </div>
+      </div>
+      <div className="field">
+        <p className="control">
+          <button className="button is-success" type="submit">
+            Login
+          </button>
+        </p>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
